7c442351b434db2b3be1e1c0637660f913e63d91
rubens2005 <rubensjrosa@gmail.com>
2026-03-20 19:53:48 -0300

    Aumentado o padding do rodapé.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rubens2005.github.io',
-  base: '/anai-rosa', // IMPORTANTE: Se for o seu site principal (seu-usuario.github.io), deixe apenas '/'
+  base: '/', // IMPORTANTE: Se for o seu site principal (seu-usuario.github.io), deixe apenas '/'
   outDir: 'dist',
 });
