1754609d2a8c159b0b5fc6461190b2ed8cd7ad48
rubens2005 <rubensjrosa@gmail.com>
2026-03-20 19:29:52 -0300

    Migrando site para Astro

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://rubens2005.github.io',
+  base: '/anai-rosa', // IMPORTANTE: Se for o seu site principal (seu-usuario.github.io), deixe apenas '/'
+  outDir: 'dist',
+});
